fd8ee744b025db8ce9db62bfb9891a1b93703b6d
barkatkamran2015 <71576244+barkatkamran2015@users.noreply.github.com>
2024-07-02 12:36:41 -0700

    Update Gallery.jsx

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -23,7 +23,9 @@ const Gallery = () => {
       <div className="app__gallery-content">
         <SubHeading title="Instagram" />
         <h1 className="headtext__cormorant">Photo Gallery</h1>
-        <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Explore the vibrant flavors and inviting ambiance 
+        of Kabul House through our photo gallery. Each image tells a story of culinary passion and unforgettable dining 
+        experiences. Enjoy a visual taste of what awaits you!</p>
         <button type="button" className="custom__button">View More</button>
       </div>
       <div className="app__gallery-images">
